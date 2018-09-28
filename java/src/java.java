a7ffb77c24d55266e78e5d244a011bc87de37823
sedzjad <stsheefe@gmail.com>
2018-09-28 14:49:37 +0200

    testing git

---
+++ java/src/java.java
@@ -0,0 +1,19 @@
+public class java {
+    public static void main(String[] args) {
+        boolean aanUit = true;
+
+        String stukjeTekst = "Wat leuk!";
+        String tweedeTekst = " Echt heh! :D";
+        String samenTekst = stukjeTekst + tweedeTekst;
+
+        int getalA = 123;
+        float getalB = 5.5f;
+        double getalC = 10.3d;
+
+
+
+        System.out.println(aanUit);
+        System.out.println(samenTekst);
+        System.out.println(getalA + getalB + getalC);
+    }
+}
